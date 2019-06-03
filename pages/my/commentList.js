79c022aa075a675a3307c6513179a984b27cc19c
saturday11 <31801248+saturday11@users.noreply.github.com>
2019-06-03 21:21:12 +0800

    Add files via upload

---
+++ pages/my/commentList.js
@@ -0,0 +1,44 @@
+// pages/my/commentList.js
+var app = getApp();
+Page({
+  data: {
+    list: [
+      {
+        date: "",
+        order_number: "",
+        content: "",
+      },
+      {
+        date: "",
+        order_number: "",
+        content: "",
+      }
+    ]
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+
+  },
+  onShow: function () {
+    this.getCommentList();
+  },
+  getCommentList: function () {
+    var that = this;
+    wx.request({
+      url: app.buildUrl("/my/comment/list"),
+      header: app.getRequestHeader(),
+      success: function (res) {
+        var resp = res.data;
+        if (resp.code != 200) {
+          app.alert({ "content": resp.msg });
+          return;
+        }
+
+        that.setData({
+          list: resp.data.list
+        });
+
+      }
+    });
+  }
+});
